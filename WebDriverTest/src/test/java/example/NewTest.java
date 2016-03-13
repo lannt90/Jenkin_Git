2fe247871c4499fa741d8817a3272826741c1d50
Yen <Yen@Yen-PC>
2016-03-13 15:06:26 +0700

    Integrate Jenkin - Git

---
+++ WebDriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,30 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+	
+  @Test
+  public void verifyTitle() {
+	  driver.get("http://www.guru99.com/selenium-tutorial.html");
+	  String title = driver.getTitle();
+	  Assert.assertTrue(title.contains("Free Selenium Tutorials"));
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new FirefoxDriver();
+	  	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
